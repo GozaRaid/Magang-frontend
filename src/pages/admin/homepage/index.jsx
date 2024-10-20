2589d9592974671d44747982f62285505c461d6b
GozaRaid <hafidzhitobaihaqi@gmail.com>
2024-10-21 01:33:26 +0700

    feat: remove admin homepage

--- src/pages/admin/homepage/index.jsx
+++
@@ -1,8 +0,0 @@
-import { EventPage } from "@/components/layouts/eventpage";
-import { withAuth } from "@/components/layouts/withAuth";
-
-export default function AdminHomepage() {
-  return <EventPage />;
-}
-
-// export default withAuth(AdminHomepage);
